76867d8c899080a1c1d3f1ab8242c4b8fccd5d7a
Berdis-Electrical <support@berdis.gr>
2025-06-27 12:38:14 +0300

    add python files
    add python files

---
+++ components/tft_espi/__init__.py
@@ -0,0 +1,3 @@
+from esphome import automation
+from esphome.components import display
+from esphome.const import CONF_ID
---
+++ components/tft_espi/component.py
@@ -0,0 +1,14 @@
+import esphome.codegen as cg
+from esphome import automation
+from esphome.const import CONF_ID
+
+tft_ns = cg.global_ns.namespace('')  # Global namespace
+TFTeSPIComponent = tft_ns.class_('TFT_eSPI_LVGL', cg.Component)
+
+CONFIG_SCHEMA = cg.Schema({
+    cg.GenerateID(): cg.declare_id(TFTeSPIComponent),
+}).extend(cg.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
