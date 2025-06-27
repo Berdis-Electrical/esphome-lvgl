2dccaa0e56d6f27ff1622274d74b736f10dcc4ac
Berdis-Electrical <support@berdis.gr>
2025-06-27 13:15:22 +0300

    c

--- components/tft_espi_lvgl/component.py
+++ components/tft_espi_lvgl/component.py
@@ -10,6 +10,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(TFTeSPIComponent),
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    yield cg.register_component(var, config)
